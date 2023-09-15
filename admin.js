6ee6a01951649906522cbd941eea04eab1239d93
Ssensen-student <139458099+Ssensen-student@users.noreply.github.com>
2023-09-15 21:56:40 +0800

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,39 @@
+const studentLink = document.getElementById("admin");
+const userManagementLink = document.getElementById("userManagement");
+const companyManagementLink = document.getElementById("companyManagement");
+const submenu = document.getElementById("submenu");
+const subSubmenu = document.getElementById("sub-submenu");
+const subSubmenu2 = document.getElementById("sub-submenu2");
+
+submenu.style.display = "none";
+subSubmenu.style.display = "none";
+
+studentLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (submenu.style.display === "none") {
+        submenu.style.display = "block";
+        subSubmenu2.style.display = "none";
+    } else {
+        submenu.style.display = "none";
+        subSubmenu.style.display = "none";
+        subSubmenu2.style.display = "none";
+    }
+});
+
+userManagementLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (subSubmenu.style.display === "none") {
+        subSubmenu.style.display = "block";
+    } else {
+        subSubmenu.style.display = "none";
+    }
+});
+
+companyManagementLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (subSubmenu2.style.display === "none") {
+        subSubmenu2.style.display = "block";
+    } else {
+        subSubmenu2.style.display = "none";
+    }
+});
