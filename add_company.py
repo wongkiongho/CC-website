1453442e0c938c07a4039113f539ac4565a56388
wongkiongho <125861978+wongkiongho@users.noreply.github.com>
2023-09-19 21:24:34 +0800

    Update add_company.py

--- add_company.py
+++ add_company.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request
 from pymysql import connections
 import os
@@ -83,4 +84,31 @@ def AddEmp():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
+
+@app.route("/viewcompanies", methods=['GET'])
+def view_companies():
+    try:
+        # Retrieve company data from the database
+        cursor = db_conn.cursor()
+        select_sql = "SELECT company_id, company_name, industry FROM company"
+        cursor.execute(select_sql)
+        company_data = cursor.fetchall()
+        cursor.close()
+
+        # Create a list to store the company details
+        companies = []
+
+        # Loop through the retrieved data and fetch S3 URLs for logos
+        for company in company_data:
+            company_id, company_name, industry = company
+            # Assuming you have a naming convention for the logo files
+            company_logo_file_name_in_s3 = f"company_id-{company_id}_logo_file"
+            # Construct the S3 URL
+            s3_location = ''  # Replace with the actual S3 location
+            logo_url = f"https://s3{s3_location}.amazonaws.com/{custombucket}/{company_logo_file_name_in_s3}"
+            companies.append({'company_name': company_name, 'industry': industry, 'logo_url': logo_url})
+
+        return render_template('admin-manage-company.html', companies=companies)
+    except Exception as e:
+        return str(e)
 
