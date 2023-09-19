fed6de2d715e6595179dc6f1df01cad98451416f
wongkiongho <125861978+wongkiongho@users.noreply.github.com>
2023-09-19 18:17:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "database-1.cizar3lczvxa.us-east-1.rds.amazonaws.com"
+customhost = "database-2.cgbw848jqsd7.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "internshipDB"
