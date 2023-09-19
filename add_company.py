47bc2724902f3ae482b1ac015ee2f0e63ecf2323
wongkiongho <125861978+wongkiongho@users.noreply.github.com>
2023-09-19 21:23:07 +0800

    Update add_company.py

--- add_company.py
+++ add_company.py
@@ -1,8 +1,12 @@
+'''
 from flask import Flask, render_template, request
 from pymysql import connections
 import os
 import boto3
 from config import *
+from uuid import uuid4
+import json
+
 
 app = Flask(__name__)
 
@@ -18,46 +22,51 @@ db_conn = connections.Connection(
 
 )
 output = {}
-table = 'employee'
+table = 'company'
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('AddEmp.html')
+    print("all modification done...")
+    return render_template('admin-manage-company.html')
 
 
-@app.route("/about", methods=['POST'])
-def about():
-    return render_template('www.tarc.edu.my')
 
+@app.route("/addcompany", methods=['POST'])
+def Addcompany():
+    # Retrieve form fields
+    company_id = str(uuid4())
+    company_name = request.form.get("companyName")
+    industry = request.form.get("industry")
+    company_desc = request.form.get("companyDesc")
+    location = request.form.get("location")
+    email = request.form.get("email")
+    contact_number = request.form.get("contactNumber")
+    positions = request.form.getlist("position[]")
+    company_detials_file = request.files.get("companyFile")
+    company_logo_file  = request.files.get("companyLogo")
+    # Serialize the positions list to JSON
+    positions_json = json.dumps(positions)
 
-@app.route("/addemp", methods=['POST'])
-def AddEmp():
-    emp_id = request.form['emp_id']
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    pri_skill = request.form['pri_skill']
-    location = request.form['location']
-    emp_image_file = request.files['emp_image_file']
-
-    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
+    insert_sql = "INSERT INTO company VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
 
-    if emp_image_file.filename == "":
-        return "Please select a file"
+    if company_detials_file.filename == "":
+        return "Please upload a company's detail file"
+    if company_logo_file.filename == "":
+        return "Please upload a company logo "
 
     try:
 
-        cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
+        cursor.execute(insert_sql, (company_id, company_name, industry, company_desc, location, email, contact_number, positions_json))
         db_conn.commit()
-        emp_name = "" + first_name + " " + last_name
-        # Uplaod image file in S3 #
-        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+        company_detials_file_name_in_s3 = "company_id-" + str(company_id) + "_details_file"
+        company_logo_file_name_in_s3 = "company_id-" + str(company_id) + "_logo_file"
         s3 = boto3.resource('s3')
-
         try:
             print("Data inserted in MySQL RDS... uploading image to S3...")
-            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
+            s3.Bucket(custombucket).put_object(Key=company_detials_file_name_in_s3, Body=company_detials_file)
+            s3.Bucket(custombucket).put_object(Key=company_logo_file_name_in_s3, Body=company_logo_file)
             bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
             s3_location = (bucket_location['LocationConstraint'])
 
@@ -69,18 +78,50 @@ def AddEmp():
             object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
                 s3_location,
                 custombucket,
-                emp_image_file_name_in_s3)
+                company_detials_file_name_in_s3)
+
+            object_url2 = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                company_logo_file_name_in_s3)
 
         except Exception as e:
             return str(e)
 
     finally:
         cursor.close()
-
+    # If it's a GET request, simply render the form
     print("all modification done...")
-    return render_template('AddEmpOutput.html', name=emp_name)
+    return render_template('admin-manage-company.html')
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+@app.route("/viewcompanies", methods=['GET'])
+def view_companies():
+    try:
+        # Retrieve company data from the database
+        cursor = db_conn.cursor()
+        select_sql = "SELECT company_id, company_name, industry FROM company"
+        cursor.execute(select_sql)
+        company_data = cursor.fetchall()
+        cursor.close()
 
+        # Create a list to store the company details
+        companies = []
+
+        # Loop through the retrieved data and fetch S3 URLs for logos
+        for company in company_data:
+            company_id, company_name, industry = company
+            # Assuming you have a naming convention for the logo files
+            company_logo_file_name_in_s3 = f"company_id-{company_id}_logo_file"
+            # Construct the S3 URL
+            s3_location = ''  # Replace with the actual S3 location
+            logo_url = f"https://s3{s3_location}.amazonaws.com/{custombucket}/{company_logo_file_name_in_s3}"
+            companies.append({'company_name': company_name, 'industry': industry, 'logo_url': logo_url})
+
+        return render_template('admin-manage-company.html', companies=companies)
+    except Exception as e:
+        return str(e)
+    
+'''
