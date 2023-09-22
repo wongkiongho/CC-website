197a776c0175c38db19429942560fc41e588247b
shuyew <126450690+shuyew@users.noreply.github.com>
2023-09-22 14:09:23 +0800

    as

--- add_student.py
+++ add_student.py
@@ -28,16 +28,6 @@ db_conn = connections.Connection(
 output = {}
 table = 'studentForm'
 
-s3 = boto3.resource('s3')
-s3_client = boto3.client('s3')
-bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-s3_location = (bucket_location['LocationConstraint'])
-
-if s3_location is None:
-    s3_location = ''
-else:
-    s3_location = '-' + s3_location
-
 @app.route("/", methods=['GET'])
 def home():
     return redirect(url_for('profile', student_id=1))
