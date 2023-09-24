0afb850b1789ebbe7f3c319e29484cfe019d1921
wongkiongho <125861978+wongkiongho@users.noreply.github.com>
2023-09-24 19:48:40 +0800

    Update add_company.py

--- add_company.py
+++ add_company.py
@@ -195,11 +195,13 @@ def editCompany(company_id):
                 # Delete previous company files from S3 and database
                 cursor.execute("SELECT file_id FROM companyFile WHERE company_id=%s", (company_id,))
                 file_ids_to_delete = [row[0] for row in cursor.fetchall()]
+                cursor.execute("SELECT file_id, file_url FROM file WHERE file_type=%s AND file_id IN %s", ("logo",tuple(file_ids_to_delete),))
+                rows = cursor.fetchall()
+                ids_to_delete = [row[0] for row in rows]
+                urls_to_delete = [row[1] for row in rows]
 
-                if file_ids_to_delete:
+                if ids_to_delete:
                     # Delete files from S3 and database
-                    cursor.execute("SELECT file_url FROM file WHERE file_type=%s AND file_id IN %s", ("logo",tuple(file_ids_to_delete),))
-                    urls_to_delete = [row[0] for row in cursor.fetchall()]
 
                     for url in urls_to_delete:
                         parsed_url = urlparse(url)
@@ -208,7 +210,7 @@ def editCompany(company_id):
                     
                     # Delete records from companyFile and file tables
                     cursor.execute("DELETE FROM companyFile WHERE company_id=%s", (company_id,))
-                    cursor.execute("DELETE FROM file WHERE file_id IN %s", (tuple(file_ids_to_delete),))
+                    cursor.execute("DELETE FROM file WHERE file_id IN %s", (tuple(ids_to_delete),))
                 # Process company logo
                 logo_content_type, _ = mimetypes.guess_type(company_logo_file.filename)
                 logo_extension = logo_content_type.split("/")[1] if logo_content_type else ""
