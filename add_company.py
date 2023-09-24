ffb2d4319e43efdd32a62ef530936d9421d77b61
wongkiongho <125861978+wongkiongho@users.noreply.github.com>
2023-09-24 19:55:57 +0800

    Update add_company.py

--- add_company.py
+++ add_company.py
@@ -228,6 +228,44 @@ def editCompany(company_id):
                 cursor.execute("INSERT INTO file (file_id, file_url, file_type, file_name, file_date) VALUES (%s, %s, %s, %s, NOW())", (logo_file_id, logo_url, "logo", company_logo_file.filename))
                 db_conn.commit()
 
+            if company_files:
+                cursor.execute("SELECT file_id FROM companyFile WHERE company_id=%s", (company_id,))
+                file_ids_to_delete = [row[0] for row in cursor.fetchall()]
+                cursor.execute("SELECT file_id, file_url FROM file WHERE file_type=%s AND file_id IN %s", ("details",tuple(file_ids_to_delete),))
+                rows = cursor.fetchall()
+                ids_to_delete = [row[0] for row in rows]
+                urls_to_delete = [row[1] for row in rows]
+
+                if ids_to_delete:
+                    # Delete files from S3 and database
+
+                    for url in urls_to_delete:
+                        parsed_url = urlparse(url)
+                        object_key = parsed_url.path.lstrip('/')
+                        s3_client.delete_object(Bucket=custombucket, Key=object_key)
+                    
+                    # Delete records from companyFile and file tables
+                    cursor.execute("DELETE FROM companyFile WHERE company_id=%s", (company_id,))
+                    cursor.execute("DELETE FROM file WHERE file_id IN %s", (tuple(ids_to_delete),))
+
+                # Process company detail files
+                for detail_file in company_files:
+                    
+                    if detail_file.filename != "":
+                        details_content_type, _ = mimetypes.guess_type(detail_file.filename)
+                        details_extension = details_content_type.split("/")[1] if details_content_type else ""
+                        detail_file_name_in_s3 = f"company_id-{company_id}_file.{details_extension}"
+
+                        s3.Bucket(custombucket).put_object(
+                            Key=detail_file_name_in_s3, 
+                            Body=detail_file, 
+                            ContentDisposition=f"attachment; filename={detail_file.filename}"
+                        )
+                        file_url = f"https://s3{s3_location}.amazonaws.com/{custombucket}/{detail_file_name_in_s3}"
+                        file_id = str(uuid4())
+                        cursor.execute("INSERT INTO companyFile (file_id, company_id) VALUES (%s, %s)", (file_id, company_id))
+                        cursor.execute("INSERT INTO file (file_id, file_url, file_type, file_name, file_date) VALUES (%s, %s, %s, %s, NOW())", (file_id, file_url, "details", detail_file.filename))
+                        db_conn.commit()
 
 
             
