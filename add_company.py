24c7ed652f68e7e3e48996f7aa7db8d157afe5bc
wongkiongho <125861978+wongkiongho@users.noreply.github.com>
2023-09-24 23:10:06 +0800

    add_company.py

--- add_company.py
+++ add_company.py
@@ -47,6 +47,20 @@ else:
 def home():
     return render_template('/student-login.html')
 
+@app.route('/student_list')
+def student_list():
+    return render_template('supervisor-studentList.html')
+
+
+@app.route('/application')
+def application():
+    return render_template('supervisor-application.html')
+
+
+@app.route('/approve_reject')
+def approve_reject():
+    return render_template('supervisor-ApproveOrReject.html')
+
 @app.route("/student-login.html", methods=['GET', 'POST'])
 def viewStudentLoginPage():
     return render_template('student-login.html')
@@ -1077,6 +1091,131 @@ def delete_supervisor(supervisor_id):
         return jsonify({"message": "Supervisor information deleted successfully"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+@app.route("/studentlist", methods=['GET'])
+def studentList():
+    try:
+        with db_conn.cursor() as cursor:
+            # Fetch all students
+            select_students_sql = "SELECT student_id, name, email, programme, cohort FROM studentDetails"
+            cursor.execute(select_students_sql)
+            student_data = cursor.fetchall()
+            
+            students = []
+            for student in student_data:
+                student_id, name, email, programme, cohort = student
+                
+                # For each student, fetch all corresponding progress reports
+                select_reports_sql = """
+                SELECT f.file_url, f.file_name 
+                FROM studentFile sf 
+                JOIN file f ON sf.file_id = f.file_id AND f.file_type = 'ProgressReport'
+                WHERE sf.student_id = %s
+                """
+                cursor.execute(select_reports_sql, (student_id,))
+                reports_data = cursor.fetchall()
+                
+                reports = [{'file_url': url, 'file_name': name} for url, name in reports_data]
+                
+                students.append({
+                    'student_id': student_id,
+                    'name': name,
+                    'email': email,
+                    'programme': programme,
+                    'cohort': cohort,
+                    'reports': reports
+                })
+
+        return render_template('supervisor-studentList.html', students=students)
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+
+@app.route("/applications", methods=['GET'])
+def applications_page():
+    try:
+        with db_conn.cursor() as cursor:
+            # Start with joining one table first
+            select_sql = """
+            SELECT a.student_id, a.company_id, a.status, a.details, s.programme
+            FROM application a
+            JOIN studentDetails s ON a.student_id = s.student_id;
+            """
+            cursor.execute(select_sql)
+            application_data = cursor.fetchall()
+
+        applications = []
+        for application in application_data:
+            student_id, company_id, status, details, programme = application
+            applications.append({
+                'student_id': student_id,
+                'company_id': company_id,
+                'status': status,
+                'details': details,
+                'programme': programme  
+            })
+
+        return render_template('supervisor-application.html', student_applications=applications)
+    except Exception as e:
+        print("Error: ", str(e))  # print the error to the console
+        return f"An error occurred: {str(e)}"
+
+@app.route("/approveOrReject", methods=['GET'])
+def approve_or_reject():
+    try:
+        with db_conn.cursor() as cursor:
+            # Example SQL, adjust as necessary
+            select_sql = """
+            SELECT a.student_id, a.company_id, a.status, a.details, f.file_url
+            FROM application a
+            LEFT JOIN studentFile sf ON a.student_id = sf.student_id
+            LEFT JOIN file f ON sf.file_id = f.file_id
+            """
+            cursor.execute(select_sql)
+            application_data = cursor.fetchall()
+
+        applications = []
+        for application in application_data:
+            student_id, company_id, status, details, file_url = application
+            applications.append({
+                'student_id': student_id,
+                'company_id': company_id,
+                'status': status,
+                'details': details,
+                'file_url': file_url
+            })
+
+        return render_template('supervisor-ApproveOrReject.html', student_applications=applications)
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+
+@app.route('/approveApplication', methods=['POST'])
+def approve_application():
+    try:
+        data = request.json
+        student_id = data.get('student_id')
+        company_id = data.get('company_id')  # Get company_id from the request data
+        with db_conn.cursor() as cursor:
+            update_sql = "UPDATE application SET status='approved' WHERE student_id=%s AND company_id=%s"
+            cursor.execute(update_sql, (student_id, company_id))  # Pass both IDs to the execute method
+            db_conn.commit()
+        return jsonify(success=True)
+    except Exception as e:
+        return jsonify(success=False, error=str(e))
+
+
+@app.route('/rejectApplication', methods=['POST'])
+def reject_application():
+    try:
+        data = request.json
+        student_id = data.get('student_id')
+        company_id = data.get('company_id')  # Get company_id from the request data
+        with db_conn.cursor() as cursor:
+            update_sql = "UPDATE application SET status='rejected' WHERE student_id=%s AND company_id=%s"
+            cursor.execute(update_sql, (student_id, company_id))  # Pass both IDs to the execute method
+            db_conn.commit()
+        return jsonify(success=True)
+    except Exception as e:
+        return jsonify(success=False, error=str(e))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
